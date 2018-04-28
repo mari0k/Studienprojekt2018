e936fa393659220b3fd09e6645e8804a3ba59f3f
Mario Kayser <36174009+mari0k@users.noreply.github.com>
2018-04-28 18:12:47 +0200

    Added Classes to compute MaximumLikelihood estimators and ValueAtRisk.

---
+++ src/MaximumLikelihood.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+/*
+ * This Class computes Maximum Likelihood estimators for mean m and standard deviation sd
+ * for a normally distributed random variable (input: some observed realizations of the random variable)
+ */
+
+public class MaximumLikelihood {
+
+	public MaximumLikelihood() {
+
+	}
+	
+	public Double[] estimate(ArrayList<Integer> realizations) {
+		Double[] estimators = new Double[3];
+		
+		if (realizations.size() <= 1) {
+			if (realizations.size() < 1) estimators[0] = 0.0;
+			else estimators[0] = realizations.get(0) * 1.0;
+			estimators[1] = 0.0;
+			estimators[2] = 0.0;
+		}
+		else {
+			int meanSum = 0;
+			for (Integer i : realizations) {
+				meanSum += i;
+			}
+			estimators[0] = (double) (meanSum / realizations.size());		// ML mean estimator
+			
+
+			double sdSum = 0;
+			for (Integer i : realizations) {
+				sdSum += (i - estimators[0])*(i - estimators[0]);
+			}
+			estimators[1] = Math.sqrt(sdSum / realizations.size());			// ML standard deviation estimator
+			estimators[2] = Math.sqrt(sdSum / (realizations.size() - 1));	// unbiased standard deviation estimator
+		}
+		
+		return estimators;		// mean, sd, unbiased sd
+	}
+	
+	
+
+}
---
+++ src/ValueAtRisk.java
@@ -0,0 +1,40 @@
+import org.apache.commons.math3.distribution.NormalDistribution;
+
+/*
+ * This Class computes the Value-at-Risk to a given confidence level alpha
+ * for a normally distributed random variable (parameters mean m and standard-deviation sd)
+ */
+
+public class ValueAtRisk {
+	
+	private double alpha = 0.99;	// confidence level
+
+	public ValueAtRisk(double alpha) {
+		this.alpha = alpha;
+	}
+	
+	
+	public double computeVaR(double mean, double sd) {		
+		if (sd > 0) {
+			NormalDistribution distribution = new NormalDistribution(mean, sd);
+			
+			return distribution.inverseCumulativeProbability(1-alpha);
+		}
+		else {
+			return mean;
+		}
+	}
+
+
+	public double getAlpha() {
+		return alpha;
+	}
+
+
+	public void setAlpha(double alpha) {
+		this.alpha = alpha;
+	}
+	
+	
+	
+}
